172470a427b51d0e837f5b96291dd047611e39f2
Mohamed-Alaa16006460 <mohamedalaa1508@gmail.com>
2025-11-11 09:14:44 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,17 +3,24 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
 
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
 
+public:
+    Instructor();
+    Instructor(string n, int i, string d, int e);
+    ~Instructor();
 
+    void setDepartment(string d);
+    void setExperienceYears(int e);
 
+    string getDepartment() const;
+    int getExperienceYears() const;
 
-
-
-
-
-
-
+    void display() const;
+};
 
 #endif
